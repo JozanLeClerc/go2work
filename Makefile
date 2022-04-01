3cd5ef65476daf64b0dc0b5369a7d88f9eff9033
JozanLeClerc <bousset.rudy@gmail.com>
2022-04-01 17:06:48 +0200

    New file, still progressing

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@
 # ========================
 #
 # go2work: Makefile
-# Wed Mar 30 13:26:48 CEST 2022
+# Fri Apr  1 17:04:04 CEST 2022
 # Joe
 #
 # GNU Makefile
@@ -28,6 +28,7 @@ TRGT_DIR	 = ./
 SRCS_NAME	 = c_defs
 SRCS_NAME	+= c_go2work
 SRCS_NAME	+= c_player
+SRCS_NAME	+= p_options
 SRCS_NAME	+= u_checks
 SRCS_NAME	+= u_prints
 SRCS_NAME	+= u_utils
@@ -55,4 +56,5 @@ run:
 # File prefixes info
 # ------------------
 # c_  -> core program
-# u_  -> utils related
+# p_  -> parsing
+# u_  -> utils
--- src/p_options.go
+++ src/p_options.go
@@ -38,93 +38,16 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * go2work: src/c_go2work.go
- * Fri Apr  1 17:03:18 CEST 2022
+ * go2work: src/p_options.go
+ * Fri Apr  1 17:03:22 CEST 2022
  * Joe
  *
- * The main.
+ * Options parsing.
  */
 
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"time"
-	"strconv"
-	"strings"
-)
-
-func main() {
-	var curr_t [3]byte
-	var dest_t [3]byte
-	var tmp int
+func parse_options() options {
 	var options options
-	log.SetPrefix(PROGNAME + ": ")
-	log.SetFlags(0)
-	if len(os.Args[0:]) == 1 {
-		log.Fatal("no arguments")
-		return
-	}
-	switch os.Args[1] {
-	case "-h", "--help":
-		print_help()
-		return
-	case "-H", "--real-help":
-		print_real_help()
-		return
-	case "-v", "--version":
-		print_version()
-		return
-	case "-t", "--test":
-		dest_t = get_test_time()
-	default:
-		if strings.Contains(os.Args[1], ":") == false {
-			log.Fatal(LOG_FORMAT)
-		}
-		str_dest_t := strings.Split(os.Args[1], ":")
-		tmp, _ = strconv.Atoi(str_dest_t[HOURS])
-		dest_t[HOURS] = byte(tmp)
-		tmp, _ = strconv.Atoi(str_dest_t[MINS])
-		dest_t[MINS] = byte(tmp)
-		dest_t[SECS] = 0
-	}
-	options = parse_options()
-	if check_time_format(dest_t) == false {
-		log.Fatal(LOG_FORMAT)
-		return
-	}
-	if check_media_player(options.media_player) == false {
-		log.Fatal("media player (" + options.media_player + ") not found")
-		return
-	}
-	if options.use_fortune == true && check_fortune() == false {
-		fmt.Println("Beware, fortune is set on but was not found")
-	}
-	curr_t = get_time()
-	print_time_left(curr_t, dest_t)
-	ticker := time.NewTicker(INTERVAL * time.Millisecond)
-	quit := make(chan struct{})
-	for {
-		select {
-		case <- ticker.C:
-			curr_t = get_time()
-			print_time_left(curr_t, dest_t)
-			if curr_t[HOURS] == dest_t[HOURS] &&
-				curr_t[MINS] == dest_t[MINS] &&
-				curr_t[SECS] == dest_t[SECS] {
-				args := append(DEF_PLAYER_OPTIONS(), DEF_FILES()[0])
-				exec_player(
-					options.use_fortune,
-					options.media_player,
-					args...,
-				)
-				return
-			}
-		case <- quit:
-			ticker.Stop()
-			return
-		}
-	}
+	return options
 }
